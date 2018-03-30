22c91643fcff9aa7ebc9f830afc2e3f2c6bebd29
Mwong <mwong153@gmail.com>
2018-03-30 02:37:07 -0700

    Rendering login/register pages

---
+++ app/src/main/java/com/example/mitchell/CustomerLoginRegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.mitchell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mitchell.cabapp.R;
+
+public class CustomerLoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_login_register);
+    }
+}
---
+++ app/src/main/java/com/example/mitchell/DriverLoginRegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.mitchell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mitchell.cabapp.R;
+
+public class DriverLoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login_register);
+    }
+}
